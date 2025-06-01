4a065a8604ff819f55e39bdfe510b4cd465b94f1
5ergey <akifyev.sergey@gmail.com>
2025-06-01 19:35:01 +0300

    Небольшие правки

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const images = ['1.png', '2.png', '3.png', '4.png', '5.png'];
   const randomIndex = Math.floor(Math.random() * images.length);
-  const imgElement = document.getElementById('random-img');
-  imgElement.src = 'img/' + images[randomIndex];
+
+  const img = document.createElement('img');
+  img.src = 'img/' + images[randomIndex];
+  img.alt = 'img';
+  img.className = 'random-img';
+
+  const wrapper = document.querySelector('.img__wrapper');
+  wrapper.appendChild(img);
 });
